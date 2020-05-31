e465dc1b8b6c0a3fd25e7cfdbf86d916a986daec
adastraz <tylergosox@gmail.com>
2020-05-30 22:13:56 -0400

    logos for welcome are in, some responsiveness taken care of

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Postgresql from '../styles/postgresql.jpg'
+import Express from '../styles/express.jpg'
+import ReactLogo from '../styles/react.png'
+import NodeJS from '../styles/node.jpg'
+import '../styles/welcome.css'
 
 const Welcome = () => {
     return(
         <>
+            <div className='logodiv'>
+                <img className='logo' src={Postgresql} />
+                <img className='logo express' src={Express} />
+                <img className='logo' src={ReactLogo} />
+                <img className='logo' src={NodeJS} />
+            </div>
         </>
     )
 }
