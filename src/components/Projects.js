d4b7bb734fe47f52088d4918958a7cdd66d386c3
adastraz <tylergosox@gmail.com>
2020-06-26 23:26:59 -0400

    touched up a little styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,7 +13,7 @@ const Projects = () => {
         <>
             <div className='projects'>
                 <div className='proj'>
-                    <h2>Schematic Capture</h2>
+                    <h2 className='titlez'>Schematic Capture</h2>
                     <p>Production Time - 2 Months</p>
                     <p>Team Size - 7 Members (1 UX, 1 iOS, 4 Web, 1 Product Manager)</p>
                     <p>This project was to allow for machine technicians a more modern way of documenting / annotating specific components of each machine. This product will go on to be a sort of 'portal' for Alloy Solution Technology to assign jobs (machines) to technicians. They are given a schematic and excel type spreadsheet of components known to be on the machine. Users are able to select images from Dropbox, to input those images into the shared Dropbox amongst employees for easy access later on. My team was in charge of bringing the codeset to be on par with the stakeholders vision, and allow for future teams to develop on top of our code to get the product production ready. 
@@ -38,7 +38,7 @@ const Projects = () => {
                     />
                 </div>
                 <div className='proj'>
-                    <h2>Droom</h2>
+                    <h2 className='titlez'>Droom</h2>
                     <p>Production Time - 1 Week</p>
                     <p>Team Size - 6 Members (5 Web, 1 Product Manager)</p>
                     <p>This project put a twist on the job search. Droom is a Tinder-like tool in order to view posted jobs. There is no real functionality for matching, but all CRUD operations are present in this application. Droom has the capability of creating two seperate user types, an employee (looking for a job) or an employer. Based on which type of user you are, when you are all registered you will see a list of users of the opposite user type. A team member and I were responsible for all state management, along with CRUD operations taken place within the application.</p>
