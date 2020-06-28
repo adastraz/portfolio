158161fc4633c4bf2ebd01c7dc7a81e11295ceb7
adastraz <tylergosox@gmail.com>
2020-06-28 13:22:29 -0400

    updated styles in projects!

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,7 +16,7 @@ const Projects = () => {
                     <h2 className='titlez'>Schematic Capture</h2>
                     <p>Production Time - 2 Months</p>
                     <p>Team Size - 7 Members (1 UX, 1 iOS, 4 Web, 1 Product Manager)</p>
-                    <p>This project was to allow for machine technicians a more modern way of documenting / annotating specific components of each machine. This product will go on to be a sort of 'portal' for Alloy Solution Technology to assign jobs (machines) to technicians. They are given a schematic and excel type spreadsheet of components known to be on the machine. Users are able to select images from Dropbox, to input those images into the shared Dropbox amongst employees for easy access later on. My team was in charge of bringing the codeset to be on par with the stakeholders vision, and allow for future teams to develop on top of our code to get the product production ready. 
+                    <p className='blockt'>This project was to allow for machine technicians a more modern way of documenting / annotating specific components of each machine. This product will go on to be a sort of 'portal' for Alloy Solution Technology to assign jobs (machines) to technicians. They are given a schematic and excel type spreadsheet of components known to be on the machine. Users are able to select images from Dropbox, to input those images into the shared Dropbox amongst employees for easy access later on. My team was in charge of bringing the codeset to be on par with the stakeholders vision, and allow for future teams to develop on top of our code to get the product production ready. 
                     </p>
                     <div className='codeStuff'>
                         <p 
@@ -41,7 +41,7 @@ const Projects = () => {
                     <h2 className='titlez'>Droom</h2>
                     <p>Production Time - 1 Week</p>
                     <p>Team Size - 6 Members (5 Web, 1 Product Manager)</p>
-                    <p>This project put a twist on the job search. Droom is a Tinder-like tool in order to view posted jobs. There is no real functionality for matching, but all CRUD operations are present in this application. Droom has the capability of creating two seperate user types, an employee (looking for a job) or an employer. Based on which type of user you are, when you are all registered you will see a list of users of the opposite user type. A team member and I were responsible for all state management, along with CRUD operations taken place within the application.</p>
+                    <p className='blockt'>This project put a twist on the job search. Droom is a Tinder-like tool in order to view posted jobs. There is no real functionality for matching, but all CRUD operations are present in this application. Droom has the capability of creating two seperate user types, an employee (looking for a job) or an employer. Based on which type of user you are, when you are all registered you will see a list of users of the opposite user type. A team member and I were responsible for all state management, along with CRUD operations taken place within the application.</p>
                     <div className='codeStuff'>
                         <p 
                             className='codebuttons application'
